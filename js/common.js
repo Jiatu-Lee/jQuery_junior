7c9556768f94f02cf95798dcdb8dd0451b460e9a
Jiatu-Lee <liuchenglu2016@hotmail.com>
2016-09-22 11:47:34 +0800

    jQuery测试
    测试prepend() 和before()d的区别。

---
+++ js/common.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Jiatu on 2016/9/21.
+ */
+$(document).ready(function () {
+
+});
+function myPrepend() {
+    $("#span1").prepend("<p>p1</p>");
+}
+function myBefore() {
+    $("#span2").before("<p>p2</p>");
+}
